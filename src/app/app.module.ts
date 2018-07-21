15dda75c5cf97fcb6547208c6670336defde54ca
Khaja Mohammed <kmoha023@fiu.edu>
2018-07-20 23:58:16 -0400

    Upgraded from Angular 4 to Angular 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -32,7 +32,7 @@ import { ShoppingListService } from "app/services/shopping-list.service";
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     RouterModule
   ],
   providers: [ShoppingListService],
